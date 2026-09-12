58ac01d04e83b93ea3ce1a3ab8739bc7f7e24855
walidoutaleb7-hub <walidoutaleb7@gmail.com>
2026-09-12 12:27:37 +0100

    Create andjava/com/weura/ai/MainActivity.java

---
+++ android/app/src/main/andjava/com/weura/ai/MainActivity.java
@@ -0,0 +1,46 @@
+package com.weura.ai;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // سيتم تغيير هذا الرابط إلى رابط WEURA المنشور
+        // بعد تجهيز الـBackend.
+        webView.loadUrl("https://YOUR-WEURA-URL.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
