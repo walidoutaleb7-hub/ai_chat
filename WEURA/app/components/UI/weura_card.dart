2348a942a5c5288c923b80e3f6f842cd35094860
walidoutaleb7-hub <walidoutaleb7@gmail.com>
2026-09-13 15:36:04 +0100

    Create weura_card.dart

---
+++ WEURA/app/components/UI/weura_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class WeuraCard extends StatelessWidget {
+  const WeuraCard({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.all(18),
+    this.margin,
+    this.borderRadius = 16,
+    this.onTap,
+  });
+
+  final Widget child;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry? margin;
+  final double borderRadius;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final content = Container(
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: const Color(0xFF101018),
+        borderRadius: BorderRadius.circular(borderRadius),
+        border: Border.all(
+          color: Colors.white.withOpacity(.065),
+        ),
+      ),
+      child: child,
+    );
+
+    if (onTap == null) {
+      return content;
+    }
+
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(borderRadius),
+        child: content,
+      ),
+    );
+  }
+}
